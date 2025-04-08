e6a52f99e198d4d85c0836c93efa15eebba0fb33
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-04-08 16:00:19 +0200

    fix: aggiunto margin header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ const title = 'Learn Web development'
 
 export default function Header() {
     return (
-        <header className="bg-body-tertiary p-4">
+        <header className="bg-body-tertiary p-4 mb-3">
             <div className="container">
                 <h1>{title}</h1>
             </div>
